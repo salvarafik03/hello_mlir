154fad19cb8784112826222b2d0712452b001f25
salvarafik03 <salva.rafik@multicorewareinc.com>
2026-03-03 14:59:27 +0530

    Enable Melo to TOSA lowering in melo-opt

--- MLIR-dummy_1/melo-opt/melo-opt.cpp
+++ MLIR-dummy_1/melo-opt/melo-opt.cpp
@@ -6,26 +6,23 @@
 #include "mlir/Parser/Parser.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
-#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
-#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
-#include "mlir/Target/LLVMIR/Export.h"
 
-#include "llvm/IR/Module.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/ToolOutputFile.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
-#include"mlir/Dialect/Func/IR/FuncOps.h"
 #include "Melo/MeloDialect.h"
 #include "Melo/MeloPasses.h"
 
-namespace cl = llvm::cl;
-static cl::opt<std::string> inputFilename(cl::Positional,
-                                          cl::desc("<input melo file>"),
-                                          cl::init("-"),
-                                          cl::value_desc("filename"));
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/SourceMgr.h"
 
+namespace cl = llvm::cl;
+
+static cl::opt<std::string> inputFilename(
+    cl::Positional,
+    cl::desc("<input melo file>"),
+    cl::init("-"),
+    cl::value_desc("filename"));
 
 int loadMLIR(mlir::MLIRContext &context,
              mlir::OwningOpRef<mlir::ModuleOp> &module) {
@@ -38,17 +35,28 @@ int loadMLIR(mlir::MLIRContext &context,
 
   llvm::SourceMgr sourceMgr;
   sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
+
   module = mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &context);
   if (!module) {
-    llvm::errs() << "Error can't load file " << inputFilename << "\n";
+    llvm::errs() << "Error: can't load file " << inputFilename << "\n";
     return 3;
   }
   return 0;
 }
 
 int processMLIR(mlir::MLIRContext &context,
-                       mlir::OwningOpRef<mlir::ModuleOp> &module) {
-  llvm::outs()<<*module;
+                mlir::OwningOpRef<mlir::ModuleOp> &module) {
+  mlir::PassManager pm(&context);
+
+  // Add Melo → TOSA lowering pass
+  pm.addPass(melo::createLowerToTosaPass());
+
+  if (mlir::failed(pm.run(module.get()))) {
+    llvm::errs() << "Failed to run Melo → TOSA lowering\n";
+    return 1;
+  }
+
+  llvm::outs() << *module;
   return 0;
 }
 
@@ -57,18 +65,22 @@ int main(int argc, char **argv) {
   mlir::registerPassManagerCLOptions();
 
   cl::ParseCommandLineOptions(argc, argv, "Melo compiler\n");
+
   mlir::MLIRContext context;
+
+  // Register dialects
   context.getOrLoadDialect<melo::MeloDialect>();
   context.getOrLoadDialect<mlir::func::FuncDialect>();
+  context.getOrLoadDialect<mlir::tosa::TosaDialect>();
+  // Register passes
+  melo::registerMeloPasses();
 
   mlir::OwningOpRef<mlir::ModuleOp> module;
-  if (int error = loadMLIR(context, module)) {
+  if (int error = loadMLIR(context, module))
     return error;
-  }
-  if (int error = processMLIR(context, module)) {
-    return error;
-  }
 
+  if (int error = processMLIR(context, module))
+    return error;
 
   return 0;
 }
